240be9d3717d40f15d224cdcdd298d3ee38b5337
tekno <aman642.g@gmail.com>
2024-12-26 23:13:15 +0530

    init

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+// import Link from "next/link";
+// import { useState } from "react";
+
+const Navbar = () => {
+  return <>REdd</>;
+};
+
+export default Navbar;
